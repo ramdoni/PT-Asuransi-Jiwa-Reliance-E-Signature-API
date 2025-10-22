455fac32d306e3f0d4aca4982f22f28fa259aad8
Ramdoni <doni.enginer@gmail.com>
2025-10-22 22:07:18 +0700

    update

--- app/Http/Middleware/CorsMiddleware.php
+++ app/Http/Middleware/CorsMiddleware.php
@@ -8,19 +8,23 @@ class CorsMiddleware
 {
     public function handle($request, Closure $next)
     {
-        $allowedOrigins = explode(',', env('FRONTEND_URL', '*'));
-        $origin = $request->headers->get('Origin');
-
         $response = $next($request);
 
-        // Tentukan origin yang diizinkan
-        if (in_array($origin, $allowedOrigins) || in_array('*', $allowedOrigins)) {
-            $response->headers->set('Access-Control-Allow-Origin', $origin ?? '*');
+        $origin = $request->header('Origin');
+
+        $allowedOrigins = [
+            'http://localhost:3000',
+            'http://relisign.entigi.co.id',
+            'https://relisign.entigi.co.id',
+        ];
+
+        if (in_array($origin, $allowedOrigins)) {
+            $response->header('Access-Control-Allow-Origin', $origin);
         }
 
-        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
-        $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
-        $response->headers->set('Access-Control-Allow-Credentials', 'true');
+        $response->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With');
+        $response->header('Access-Control-Allow-Credentials', 'true');
 
         return $response;
     }
